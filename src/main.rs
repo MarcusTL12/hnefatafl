8f808e5db1709f8fec6d39c6504d0d40c6168a6b
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-01-03 13:56:50 +0100

    Flkhlskdfjh356

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+#![feature(iter_array_chunks)]
+
+use std::fmt::Display;
+
+use bitarray::BitArray;
+
+const N: usize = 256usize.div_ceil(usize::BITS as usize);
+
+struct BoardState(pub BitArray<N>);
+
+impl Display for BoardState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut first_row = true;
+
+        for c in self
+            .0
+            .into_iter()
+            .array_chunks::<2>()
+            .take(121)
+            .array_chunks::<11>()
+        {
+            let mut first_col = true;
+
+            if first_row {
+                writeln!(f, "╔═╤═╤═╤═╤═╤═╤═╤═╤═╤═╤═╗")?;
+            } else {
+                writeln!(f, "╟─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─╢")?;
+            }
+
+            for x in c {
+                if first_col {
+                    write!(f, "║")?;
+                } else {
+                    write!(f, "│")?;
+                }
+
+                write!(f, "{}", match x {
+                    [false, false] => " ",
+                    [true, false] => "♔",
+                    [false, true] => "♟",
+                    [true, true] => "♙",
+                    _ => panic!(),
+                })?;
+
+                first_col = false;
+            }
+
+            writeln!(f, "║")?;
+
+            first_row = false;
+        }
+
+        writeln!(f, "╚═╧═╧═╧═╧═╧═╧═╧═╧═╧═╧═╝")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = BoardState(BitArray::new());
+
+    println!("{state}");
 }
