bdab59e72ac018f8654fe9fa58e007a009d34740
MarcusTL12 <marcuspaafjellet@gmail.com>
2025-01-11 00:27:59 +0100

    Basic minimax

--- src/bot.rs
+++ src/bot.rs
@@ -1,12 +1,10 @@
-use bitarray::BitArray;
-
-use crate::board::{BoardState, Faction, Piece};
+use crate::board::{BoardState, Faction, Piece, W};
 
 impl BoardState {
     pub fn all_moves(
         &self,
         turn: Faction,
-    ) -> impl Iterator<Item = BitArray<2>> {
+    ) -> impl Iterator<Item = [[u16; 2]; 2]> {
         self.0
             .nbits_iter::<2>()
             .map(|x| Piece::try_from(x).unwrap())
@@ -19,7 +17,74 @@ impl BoardState {
                     .map(move |(j, x)| ([i as u16, j as u16], x))
             })
             .filter_map(move |(coord, x)| {
-                (x.try_into() == Ok(turn)).then_some(self.moves_from(coord))
+                (x.try_into() == Ok(turn))
+                    .then_some((coord, self.moves_from(coord)))
             })
+            .flat_map(|(from, legal_moves)| {
+                legal_moves
+                    .trues_iter()
+                    .map(|i| [(i / W) as u16, (i % W) as u16])
+                    .map(move |to| [from, to])
+            })
+    }
+
+    pub fn eval(self, turn: Faction, depth: u32) -> f64 {
+        if depth == 0 {
+            return self
+                .0
+                .nbits_iter::<2>()
+                .take(121)
+                .map(|x| match Piece::try_from(x).unwrap().try_into() {
+                    Ok(Faction::White) => 1.0,
+                    Ok(Faction::Black) => -1.0,
+                    _ => 0.0,
+                })
+                .sum();
+        }
+
+        let evals_iter = self.all_moves(turn).map(|[from, to]| {
+            let mut new_board = self;
+            if new_board.do_move(from, to) {
+                match turn {
+                    Faction::Black => -f64::INFINITY,
+                    Faction::White => f64::INFINITY,
+                }
+            } else {
+                new_board.eval(turn.other_faction(), depth - 1)
+            }
+        });
+
+        match turn {
+            Faction::Black => evals_iter.min_by(|a, b| a.total_cmp(b)),
+            Faction::White => evals_iter.max_by(|a, b| a.total_cmp(b)),
+        }
+        .expect("No legal moves!")
+    }
+
+    pub fn best_move(self, turn: Faction, depth: u32) -> ([[u16; 2]; 2], f64) {
+        let evals_iter = self.all_moves(turn).map(|[from, to]| {
+            let mut new_board = self;
+            (
+                [from, to],
+                if new_board.do_move(from, to) {
+                    match turn {
+                        Faction::Black => -f64::INFINITY,
+                        Faction::White => f64::INFINITY,
+                    }
+                } else {
+                    new_board.eval(turn.other_faction(), depth)
+                },
+            )
+        });
+
+        match turn {
+            Faction::Black => {
+                evals_iter.min_by(|(_, a), (_, b)| a.total_cmp(b))
+            }
+            Faction::White => {
+                evals_iter.max_by(|(_, a), (_, b)| a.total_cmp(b))
+            }
+        }
+        .expect("No best move!")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(iter_array_chunks)]
 
-use std::env;
+use std::{env, time::Instant};
 
 use board::{BoardState, Faction};
 use game::GameState;
@@ -16,18 +16,29 @@ fn main() {
 
     match args.next().as_deref() {
         Some("test") => {
+            // let mut board = BoardState::new();
+
+            // board.set_2d([3, 3], Piece::King);
+            // board.set_2d([4, 3], Piece::Black);
+            // board.set_2d([3, 4], Piece::Black);
+
+            // board.set_2d([4, 3], Piece::Black);
+            // board.set_2d([3, 5], Piece::Black);
+
+            // board.set_2d([4, 4], Piece::White);
+            // board.set_2d([0, 5], Piece::White);
+
             let board = BoardState::standard_setup();
 
-            let tmp: Vec<_> = board.all_moves(Faction::Black).collect();
+            println!("{board}");
 
-            println!("{}", tmp.len());
+            let n: u32 = args.next().map(|x| x.parse().unwrap()).unwrap_or(0);
 
-            println!("{tmp:?}");
+            let t = Instant::now();
+            let m = board.best_move(Faction::Black, n);
+            let t = t.elapsed();
 
-            println!(
-                "{}",
-                tmp.into_iter().map(|x| x.count_ones()).sum::<usize>()
-            );
+            println!("Best move: {m:?}, took: {t:.2?}");
         }
         _ => {
             let mut game = GameState::new();
